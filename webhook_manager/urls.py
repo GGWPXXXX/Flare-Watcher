04358ba2999228565823b175f83a13c2e9687127
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-04-14 23:17:21 +0700

    Refactor get_user_id URL pattern in webhook_manager/urls.py

--- webhook_manager/urls.py
+++ webhook_manager/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('line_webhook/', views.line_webhook, name='line_webhook'),
-    path('get_user_id/<str:user_id>/<str:reply_token>/', views.get_user_id, name='get_user_id'),
+    path('get_user_id/<str:user_id>/', views.get_user_id, name='get_user_id'),
 ]
