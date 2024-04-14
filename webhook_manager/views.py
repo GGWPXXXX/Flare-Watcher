8168239d9edaab56cdc12ae7ffff4b9c814ace8a
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-04-14 13:11:49 +0700

    Refactor line_webhook view to handle POST requests and save user_id to the database in views.py

--- webhook_manager/views.py
+++ webhook_manager/views.py
@@ -5,18 +5,29 @@ from decouple import config
 from .models import LineWebhook
 
 
+import json
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+# Assuming LineWebhook is your Django model for storing webhook data
+from .models import LineWebhook
+
+
 @csrf_exempt
 def line_webhook(request):
     if request.method == "POST":
-        # data = json.loads(request.body.decode("utf-8"))
-        # event_type = data['events'][0]['type']
-        # user_id = data['events'][0]['source']['userId']
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+            user_id = data['source']['userId']
+            LineWebhook.objects.create(user_id=user_id)
 
-        # save data to database
-        # LineWebhook.objects.create(event_type=event_type, user_id=user_id)
-
-        # return success response
-        return HttpResponse(status=200)
+            # Return success response
+            return HttpResponse(status=200)
+        except KeyError:
+            # if the required key is not present in the JSON data
+            return HttpResponse(status=400, content="Invalid JSON data")
+        except json.JSONDecodeError:
+            # uf the request body is not valid JSON
+            return HttpResponse(status=400, content="Invalid JSON format")
     else:
         return HttpResponse(status=405, content="Method not allowed")
 
@@ -32,11 +43,12 @@ def get_line_user_id(request):
     # if any other request method is used
     return HttpResponse(status=400, content="Invalid request method")
 
+
 @csrf_exempt
 def test_post(request):
     if request.method == "POST":
-            greeting = request.POST.get("greeting", "")
-            LineWebhook.objects.create(event_type="test", user_id=greeting)
-            return HttpResponse(status=200, content="Success")
+        greeting = request.POST.get("greeting", "")
+        LineWebhook.objects.create(event_type="test", user_id=greeting)
+        return HttpResponse(status=200, content="Success")
     else:
         return HttpResponse(status=405, content="Method not allowed")
