d1db984a8382c65297b8f4f64d6b7b8399e671fe
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-04-16 21:12:08 +0700

    completed predict.py

--- prediction/predict.py
+++ prediction/predict.py
@@ -1,12 +1,16 @@
+import numpy as np
 from ultralytics import YOLO
 import os
 import requests
+import pandas as pd
+import pickle
 
 yolo_model_path = "../Flare-Watcher/prediction/model/yolo_object_detection.pt"
 yolo_model_url = "https://media.githubusercontent.com/media/GGWPXXXX/Flare-Watcher/main/best.pt"
 
 
 def set_up():
+    """ Check to see whether the model file exists locally, if not download it from the url """
     # check if the model file exists locally
     if not os.path.exists(yolo_model_path):
         print("Downloading model file...")
@@ -15,11 +19,23 @@ def set_up():
             f.write(response.content)
 
 
-def image_prediction(image_path):
+def image_prediction(image_path) -> dict:
+    """ Predict the image using YOLO model to detect fire"""
     set_up()
     # load and predict
     model = YOLO('best.pt')
-    results = model(image_path)
-    for i in results:
-        i.show()
-        
+    return model(image_path)
+
+
+def load_random_forest_model() -> object:
+    """ Load the random forest model """
+    with open('../Flare-Watcher/prediction/model/random_forest_model.pkl', 'rb') as f:
+        clf2 = pickle.load(f)
+    return clf2
+
+
+def sensor_prediction(sensor_data: list) -> int:
+    """ Predict whether the sensor data indicates a fire or not"""
+    feature_names = ['Humidity[%]', 'TVOC[ppb]', 'eCO2[ppm]',
+                     'Raw H2', 'Raw Ethanol', 'Pressure[hPa]']
+    return load_random_forest_model().predict(pd.DataFrame(np.array(sensor_data).reshape(1, -1), columns=feature_names))[0]
