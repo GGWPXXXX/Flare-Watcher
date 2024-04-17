02979796a0e119d1e529ae3a4475bb00bc05c765
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-04-17 10:21:59 +0700

    completed sensor_prediction_view

--- prediction/views.py
+++ prediction/views.py
@@ -1,13 +1,14 @@
 import json
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from prediction.predict import sensor_prediction
+
 
 @csrf_exempt
 def sensor_prediction_view(request):
     if request.method == "POST":
         # extract json object and pass it to the prediction function
-        result = sensor_prediction([i for i in json.loads(request.body.decode("utf-8")).values()])
-        return HttpResponse(status=200, content=json.dumps({"prediction": result}))
-    return HttpResponse(status=405, content="Method Not Allowed") 
-    
+        result = sensor_prediction(
+            [i for i in json.loads(request.body.decode("utf-8")).values()])
+        return HttpResponse(status=200, content=result)
+    return HttpResponse(status=405, content="Method Not Allowed")
