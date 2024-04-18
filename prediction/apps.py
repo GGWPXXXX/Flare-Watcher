4528a0e4b61c2cf2558d0a2a6992e14931a4dd3a
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-04-18 13:37:47 +0700

    Add threading for MQTT client in prediction/apps.py

--- prediction/apps.py
+++ prediction/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import paho.mqtt.client as mqtt
 from decouple import config
+import threading
 
 
 class PredictionConfig(AppConfig):
@@ -8,23 +9,29 @@ class PredictionConfig(AppConfig):
     name = 'prediction'
     
     def ready(self):
-        self.mqtt_client = start_mqtt_client()
+        self.mqtt_client = start_mqtt_client_thread()
 
-    
-def start_mqtt_client():
+def start_mqtt_client_thread():
     mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
     
     mqtt_client.username_pw_set(config('MQTT_USER'), config('MQTT_PASS'))
     mqtt_client.connect(config('MQTT_BROKER'), config('MQTT_PORT', cast=int), 60)
-    mqtt_client.loop_start()
+    
+    # Start MQTT client in a separate thread
+    mqtt_thread = threading.Thread(target=mqtt_client.loop_forever)
+    mqtt_thread.daemon = True  # Daemonize thread
+    mqtt_thread.start()
     
     return mqtt_client
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to MQTT broker with result code " + str(rc))
-    client.subscribe("b6510545608/year_project")
+    if rc == 0:
+        print("Connected to MQTT broker")
+        client.subscribe("b6510545608/year_project")
+    else:
+        print(f"Failed to connect to MQTT broker with result code {rc}")
 
 def on_message(client, userdata, msg):
     print(f"Received message on topic '{msg.topic}': {msg.payload.decode()}")
--- prediction/urls.py
+++ prediction/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 
-app_name="webhook_manager"
+app_name="prediction"
 urlpatterns = [
     path('sensor_prediction/', views.sensor_prediction_view, name='sensor_prediction_view'),
     path('image_prediction/', views.image_prediction_view, name='image_prediction_view'),
